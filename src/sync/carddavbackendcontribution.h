72bdbacbbb5340bb821bf1ad6f2147212bf7bda8
clinton@concernednetizen.com <Clinton>
2026-05-14 16:27:56 -0400

    K.8a T2: Extract CardDavBackendContribution to public header

---
+++ src/sync/carddavbackendcontribution.h
@@ -0,0 +1,22 @@
+#ifndef KALBURATOR_SYNC_CARDDAVBACKENDCONTRIBUTION_H
+#define KALBURATOR_SYNC_CARDDAVBACKENDCONTRIBUTION_H
+
+#include "backendcontribution.h"
+#include "carddavprovider.h"
+#include "iprovider.h"
+
+namespace Kalburator::Sync {
+
+class CardDavBackendContribution : public BackendContribution {
+public:
+    QString backendType() const override { return QStringLiteral("carddav"); }
+    QList<Shape::Shape> nativeShapes() const override { return {}; }
+    std::unique_ptr<IProvider> createProvider(QObject *parent) const override
+    {
+        return std::make_unique<CardDavProvider>(parent);
+    }
+};
+
+} // namespace Kalburator::Sync
+
+#endif
